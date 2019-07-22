c6c491b3fbaa984d53517fda8c70f4f648b0dd9e
Flappo <flappopk@gmail.com>
2019-07-22 19:40:18 +0200

    BrushBlade Commit
    Added BrushBlade in Assets with texture, decals and DecalChange script.
    Added it in the scene as child of Markus's right hand and adjusted the position.

---
+++ Progetto/Assets/Player/Scripts/DecalChange.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecalChange : MonoBehaviour
+{
+    public GameObject brush;
+    public Material mate;
+
+    public Texture green;
+    public Texture red;
+    public Texture blue;
+
+    private int count = 0;
+ 
+    // Start is called before the first frame update
+    void Start()
+    { 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            count++;
+
+            if (count >= 3)
+                count = 0;
+
+            switch (count)
+            {
+                case 0:
+                    mate.SetTexture("_DecalTex", green);
+                    break;
+                case 1:
+                    mate.SetTexture("_DecalTex", red);
+                    break;
+                case 2:
+                    mate.SetTexture("_DecalTex", blue);
+                    break;
+            }
+
+        }
+
+
+    }
+}
+    
